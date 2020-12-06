5e0dace68ccfad8c0ca44d99c8c5109a7911af84
MrDionis <mrdionis@mail.com>
2020-12-06 12:22:22 +0300

    few features

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Простой микросервис Fast API для проверки IP Microsoft на Azure IP c деплоем на Heroku  
+# Простой микросервис Fast API для проверки IP Microsoft на Azure IP c деплоем на Heroku
+
+данные тут: https://www.microsoft.com/en-us/download/details.aspx?id=56519
+
+интерфейс:  https://check-azure-ip.herokuapp.com/?ip=51.137.62.148  
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import ipaddress, pickle
+import ipaddress, sqlite3
 
 from typing import Optional
 from fastapi import FastAPI, Query
@@ -10,18 +10,31 @@ app = FastAPI(
     docs_url=None,
     redoc_url=None)
 
-with open('list_ip_azure.pickle', 'rb') as f:
-        azure_ip = pickle.load(f)
-        azure_ip = [ipaddress.ip_network(x) for x in azure_ip]
+conn = sqlite3.connect('azure_rang.db', check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("select * from networks")
+azure_ip = [(ipaddress.ip_network(x[0]), x[1]) for x in cursor.fetchall()]
 
 @app.get("/")
 def hello(ip: Optional[str] = Query(None, regex="^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")):
 
     if not ip:
-        return {"message":"Hello! It is checker AzureIP"}
+        return {"msg": "Hello! It is checker AzureIP"}
 
     ip = ipaddress.ip_address(ip)
     for network in azure_ip:
-        if ip in network:
-            return {"azure": True, "message": f"{str(ip)} in {str(network)} Azure's network"}
-    return {"azure": False, "message": f"{str(ip)} not is Azure's IP"}
+        if ip in network[0]:
+            ranges = ', '.join(network[1].split())
+            cursor.execute(f"select * from ranges where id in ({ranges})")
+            return {
+                "azure": True,
+                "msg": f"{str(ip)} in {str(network[0])} Azure's network(-s)",
+                "network": [{
+                    "name": x[1],
+                    "region": x[2],
+                    "sys_service": x[3],
+                    "features": x[4]
+                } for x in cursor.fetchall()]
+            }
+    return {"azure": False, "msg": f"{str(ip)} not is Azure's IP"}
